53f262bcd4f756e9466e884a12851e3f301424cb
Lindsay Thomas <lindsay_thomas@esri.com>
2013-08-26 08:40:08 -0400

    updated LG config

--- js/configLG.js
+++ js/configLG.js
@@ -24,54 +24,64 @@ dojo.declare("js.Config", null, {
     //
     // 1.  Specify application title                  - [ Tag(s) to look for: ApplicationName ]
     // 2.  Set path for application icon              - [ Tag(s) to look for: ApplicationIcon ]
-    // 3.  Set splash screen message                  - [ Tag(s) to look for: SplashScreen ]
-    // 4.  Set URL for help page                      - [ Tag(s) to look for: HelpURL ]
-    //
-    // 5.  Specify URLs for basemaps                  - [ Tag(s) to look for: BaseMapLayers ]
-    // 6.  Set initial map extent                     - [ Tag(s) to look for: DefaultExtent ]
-    //
-    // 7.  Tags for using map services:
-    // 7a. Specify URLs and attributes for operational layers
-    //                                                - [ Tag(s) to look for: FacilityLayer,CommentsLayer,ReferenceOverlayLayer,FacilityId,FeatureName ]
-    // 7b. Customize info-Window settings             - [ Tag(s) to look for: InfoWindowHeader, InfoWindowContent ]
-    // 7c. Customize info-Popup settings              - [ Tag(s) to look for: InfoPopupFieldsCollection, Activities ]
-    // 7d. Customize activity search settings         - [ Tag(s) to look for: ActivitySearch ]
-    // 7e. Customize info-Popup size                  - [ Tag(s) to look for: InfoPopupHeight, InfoPopupWidth ]
-    // 7f. Customize data formatting                  - [ Tag(s) to look for: ShowNullValueAs, FormatDateAs ]
-    //
-    // 8. Customize buffer settings                   - [ Tag(s) to look for: BufferDistance ]
-    // 9. Customize address search settings           - [ Tag(s) to look for: LocatorURL, LocatorNameFields, , LocatorDefaultAddress,LocatorDefaultFeature, LocatorMarkupSymbolPath, AddressMatchScore,LocatorRippleSize ]
-    //LocatorFields
-    // 10. Set URL for geometry service                - [ Tag(s) to look for: GeometryService ]
-    //
-    // 11. Customize routing settings for directions  - [ Tag(s) to look for: RouteServiceURL, RouteColor, RouteWidth, GetDirections ]
-    //
-    // 12. Configure data to be displayed on the bottom panel
-    //                                                - [ Tag(s) to look for: InfoBoxWidth]
-    //
-    // 13. Specify URLs for map sharing               - [ Tag(s) to look for: MapSharingOptions,TinyURLServiceURL, TinyURLResponseAttribute, FacebookShareURL, TwitterShareURL, ShareByMailLink ]
-    //
-    // 14. Set the sequence for info-pods             - [ Tag(s) to look for: Order]
-    //
+    // 3.  Set path for application favicon           - [ Tag(s) to look for: ApplicationFavicon ]
+    // 4.  Set path for application theme             - [ Tag(s) to look for: ApplicationTheme ]
+
+    // 5.  Set splash screen message                  - [ Tag(s) to look for: SplashScreen ]
+    // 6.  Set URL for help page                      - [ Tag(s) to look for: HelpURL ]
+
+    // 7.  Specify URLs for basemaps                  - [ Tag(s) to look for: BaseMapLayers ]
+    // 8.  Set initial map extent                     - [ Tag(s) to look for: DefaultExtent ]
+
+    // 9.  Tags for using map services:
+    // 9a. Specify URLs and attributes for operational layers
+    //                                                - [ Tag(s) to look for: FacilityLayer, PrimaryKeyForFeatures, CommentsLayer, ForeignKeyforComments, FeatureName, ReferenceOverlayLayer ]
+    // 9b. Customize info-Window settings             - [ Tag(s) to look for: InfoWindowHeader, InfoWindowContent ]
+    // 9c. Customize info-Popup settings              - [ Tag(s) to look for: InfoPopupFieldsCollection, Activities ]
+    // 9d. Customize info-Popup size                  - [ Tag(s) to look for: InfoPopupHeight, InfoPopupWidth ]
+    // 9e. Customize data formatting                  - [ Tag(s) to look for: ShowNullValueAs, FormatDateAs ]
+
+    // 10. Customize buffer settings                  - [ Tag(s) to look for: BufferDistance ]
+    // 11. Customize directions settings              - [ Tag(s) to look for: GetDirectionsMobile, GetDirectionsDesktop, GetDirections, ApproximateValue ]
+
+    // 12a. Customize address search settings         - [ Tag(s) to look for: DefaultLocatorSymbol, MarkupSymbolSize, Locators, LocatorDefaultAddress, LocatorParameters, LocatorURL, CandidateFields, 
+    //                                                                        DisplayField, AddressMatchScore, LocatorFieldName, LocatorFieldValues, CountyFields, MaxResults ]
+    // 12b. Customize facility search settings        - [ Tag(s) to look for: DisplayText, LocatorDefaultFeature ]
+    // 12c. Customize activity search settings        - [ Tag(s) to look for: DisplayText ]
+
+    // 13. Customize ripple size settings             - [ Tag(s) to look for: LocatorRippleSize ]
+    // 14. Customize zoom level settings              - [ Tag(s) to look for: ZoomLevel ]
+    // 15. Customize database fields settings         - [ Tag(s) to look for: DatabaseFields ]
+    // 16. Customize Comments infoPopup settings      - [ Tag(s) to look for: CommentsInfoPopupFieldsCollection ]
+    // 17. Set URL for geometry service               - [ Tag(s) to look for: GeometryService ]
+
+    // 18. Customize routing settings for directions  - [ Tag(s) to look for: RouteServiceURL, RouteColor, RouteWidth ]
+
+    // 19. Configure data to be displayed on the bottom panel
+    //                                                - [ Tag(s) to look for: InfoBoxWidth, Order]
+
+    // 20. Specify URLs for map sharing               - [ Tag(s) to look for: MapSharingOptions, TinyURLServiceURL, TinyURLResponseAttribute, FacebookShareURL, TwitterShareURL, ShareByMailLink ]
+
+    
     // ------------------------------------------------------------------------------------------------------------------------
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------------------------------------------------------
     // Set application title
-    ApplicationName: "Park & Recreation Finder",
+    ApplicationName: "Configurable Place Finder",
 
     // Set application icon path
-    ApplicationIcon: "images/appIcon.png",
+    ApplicationIcon: "images/i_hydro.png",
 
     // Set application Favicon path
-    ApplicationFavicon: "images/appIcon.ico",
+    ApplicationFavicon: "images/i_hydro.ico",
 
     // Set application theme
-    ApplicationTheme: "styles/greenTheme.css",
+    ApplicationTheme: "styles/blueTheme.css",
 
     // Set splash window content - Message that appears when the application starts
     SplashScreen: {
-        Message: "<b>Welcome to Park Finder</b> <br/> <hr/> <br/> The <b>Park Finder</b> application helps citizens locate a park or recreation facility and obtain information about recreation activities in their community.  <br/><br/>To locate a park, simply enter an address or activity in the search box, or use your current location. The park(s) or recreation area(s) will then be highlighted on the map and relevant information about available recreation activities presented to the user.",
-        isVisible: true
+        Message: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.",
+        isVisible: false
     },
 
     // Set URL of help page/portal
@@ -94,7 +104,7 @@ dojo.declare("js.Config", null, {
         MapURL: "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"
     }],
 
-    // Initial map extent. Use comma (,) to separate values and don t delete the last comma
+    // Initial map extent. Use comma (,) to separate values and don't delete the last comma
     DefaultExtent: "-9991781.18961914, 4083344.0852194074, -9160146.321876464, 4494881.045506775",
 
     // ------------------------------------------------------------------------------------------------------------------------
@@ -114,8 +124,8 @@ dojo.declare("js.Config", null, {
         //URL used for doing query task on the comments layer
         URL: "http://50.18.115.76:6080/arcgis/rest/services/WaterAccess/FeatureServer/1"
     },
-    //Set the primary key attribute for comments
-    PrimaryKeyForComments: "${id}",
+    //Set the foreign key attribute for comments
+    ForeignKeyforComments: "${id}",
 
     //Set the name attribute for features
     FeatureName: "${Name}",
@@ -139,7 +149,7 @@ dojo.declare("js.Config", null, {
         InfoWindowHeaderText: "Facility Info"
     }],
 
-    // Choose content/fields for the info window
+    // Set content/fields for the callout window in Smartphone's
     InfoWindowContent: [{
         FieldName: "${Region}",
         Alias: "Region"
@@ -245,18 +255,15 @@ dojo.declare("js.Config", null, {
     //set distance in miles for drawing the buffer
     BufferDistance: "2",
 
-    //Set the locator ripple size
-    LocatorRippleSize: 40,
+    //Set this variable to true/false to enable/disable directions
+    //if this master variable is set to false directions cannot be enabled for any of the devices
+    GetDirections: true,
 
     //Set this variable to true/false to enable/disable directions for Mobile/tablet
     GetDirectionsMobile: true,
 
     //Set this variable to true/false to enable/disable directions for desktop
     GetDirectionsDesktop: true,
-
-    //Set this variable to true/false to enable/disable directions
-    //if this master variable is set to false directions cannot be enabled for any of the devices
-    GetDirections: true,
 
     //Set this value to display text besides calculated distances in search results
     ApproximateValue: "approx",
@@ -275,7 +282,7 @@ dojo.declare("js.Config", null, {
         Locators: [{
             DisplayText: "Location",
             LocatorDefaultAddress: "Lake Echo Rd Tracy City TN 37387",
-            LocatorParamaters: {
+            LocatorParameters: {
                 SearchField: "text",
                 SearchResultField: "outFields",
                 SearchCountField: "maxLocations",
@@ -295,7 +302,7 @@ dojo.declare("js.Config", null, {
                 LocatorFieldValue: 'POI',
                 FieldName: 'Type',
                 Value: 'county',
-                countySearch: true
+                CountySearch: true
             },
             MaxResults: 200
         }, {
@@ -305,6 +312,10 @@ dojo.declare("js.Config", null, {
             DisplayText: "Activity"
         }]
     },
+
+    //Set the locator ripple size(in pixels) 
+    LocatorRippleSize: 40,
+
     //Set the Zoom Level
     ZoomLevel: 12,
     // Define the database field names
@@ -340,18 +351,22 @@ dojo.declare("js.Config", null, {
     // Set color for the route symbol
     RouteColor: "#7F7FFE",
 
-    // Set width of the route
+    // Set width(in pixels) of the route
     RouteWidth: 6,
 
     // ------------------------------------------------------------------------------------------------------------------------
     // SETTINGS FOR INFO-PODS ON THE BOTTOM PANEL
     // ------------------------------------------------------------------------------------------------------------------------
-    // Set width of the pods in the bottom panel
+    // Set width(in pixels) of the pods in the bottom panel
     InfoBoxWidth: 422,
+
+    // Set sequence for info pods in the bottom panel
+    Order: ["search", "facility", "directions", "photogallery", "comments"],
 
     // ------------------------------------------------------------------------------------------------------------------------
     // SETTINGS FOR MAP SHARING
     // ------------------------------------------------------------------------------------------------------------------------
+
     // Set URL for TinyURL service, and URLs for social media
     MapSharingOptions: {
         TinyURLServiceURL: "http://api.bit.ly/v3/shorten?login=esri&apiKey=R_65fd9891cd882e2a96b99d4bda1be00e&uri=${0}&format=json",
@@ -359,10 +374,5 @@ dojo.declare("js.Config", null, {
         FacebookShareURL: "http://www.facebook.com/sharer.php?u=${0}&t=Configurable%20Place%20Finder",
         TwitterShareURL: "http://mobile.twitter.com/compose/tweet?status=Configurable%20Place%20Finder ${0}",
         ShareByMailLink: "mailto:%20?subject=Check%20out%20this%20map!&body=${0}"
-    },
-    // ------------------------------------------------------------------------------------------------------------------------
-    // SETTINGS FOR INFOPODS
-    // ------------------------------------------------------------------------------------------------------------------------
-    // Set sequence for info pods in the bottom panel
-    Order: ["search", "facility", "directions", "photogallery", "comments"]
+    }
 });
