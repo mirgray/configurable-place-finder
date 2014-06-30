c4e0eb6f07ebb848de91bf3acbf4f7e6f89fc2f9
Lindsay Thomas <lindsay_thomas@esri.com>
2014-06-30 10:42:58 -0400

    Updated Geoenrichment comments on credit usage

--- js/config.js
+++ js/config.js
@@ -240,7 +240,9 @@ dojo.declare("js.Config", null, {
     //set distance in miles for drawing the buffer
     BufferDistance: "2",
 
-    //Set this variable to true/false to enable/disable directions
+    //Set this variable to true/false to enable/disable directions and will consume credits
+    //Enabling the "GetDirections" parameter will draw routes and provide directions using the ArcGIS Online World Route Service.
+    //The World Route Service is an ArcGIS Online for organizations subscription service that uses credits when routes are generated.
     //if this master variable is set to false directions cannot be enabled for any of the devices
     GetDirections: true,
 
